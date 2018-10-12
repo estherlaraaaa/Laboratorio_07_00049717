c6de112b6ec04b2bcc052dc43ef01811a771e7e7
--unset-all <--unset-all>
2018-10-12 10:02:49 -0600

    comenzando

---
+++ Practica/asset/js/script.js
@@ -0,0 +1,9 @@
+var container; 
+
+container = document.getElementById("c1"); 
+
+container.innerText("¡Hola Mundo!"); 
+
+container.style.backgroundColor= "#000000"; 
+
+container.style.color="#1cb723"; 
