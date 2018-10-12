7917d746ed230ae46fe8f3b9e9ab2ca4bad23b58
--unset-all <--unset-all>
2018-10-12 10:26:52 -0600

    sdfvdsvgsf

--- Practica/asset/js/script.js
+++ Practica/asset/js/script.js
@@ -1,9 +1,22 @@
 var container; 
 
-container = document.getElementById("c1"); 
+container = document.getElementById('c1'); 
 
-container.innerText("¡Hola Mundo!"); 
+container.innerText="¡Hola Mundo!"; 
 
 container.style.backgroundColor= "#000000"; 
 
 container.style.color="#1cb723"; 
+
+container.style.width= "200px"; 
+
+container.style.height="200px"; 
+
+container.innerHTML="<input type=”text” placeholder=’escriba su texto’>";
+
+var containers= document.getElementsByClassName('container c2');
+
+for (let i=0; i<containers.length; i++){
+    containers[i].style.backgroundColor="#009999";
+    containers[i].innerHTML= "<input type='text' placeholder='Escriba su texto'>"; 
+}
